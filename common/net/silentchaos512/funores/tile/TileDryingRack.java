30efa27b20edc0a29fbb6f18b8ec2b764a274ebc
SilentChaos512 <sworkman256@gmail.com>
2016-02-12 00:05:53 -0600

    Maybe fixed drying rack desync?

--- common/net/silentchaos512/funores/tile/TileDryingRack.java
+++ common/net/silentchaos512/funores/tile/TileDryingRack.java
@@ -164,6 +164,10 @@ public class TileDryingRack extends TileEntity implements ITickable, IInventory 
     if (tags.hasKey("ItemStack")) {
       stack = ItemStack.loadItemStackFromNBT(tags.getCompoundTag("ItemStack"));
     }
+
+    if (getWorld().isRemote) {
+      getWorld().markBlockForUpdate(getPos());
+    }
   }
 
   @Override
